eaf198608b2f73e65ae26fd5d8b8bb6a74e4c9df
Tausifqureshi <tausifqureshi504@gmail.com>
2024-07-25 12:57:25 +0530

    HTML-Iframe-Tag

--- HTML-IframTag.js
+++ HTML-IframTag.js
@@ -44,4 +44,4 @@
 
 //<iframe width="560" height="315" src="https://www.youtube.com/embed/xyz123" frameborder="0" allowfullscreen></iframe>
 
-//<iframe width="560" height="315" src="https://www.youtube.com/embed/txMuWyhHUPk?si=0eDDyRwneEPDSbgw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+//<iframe width="560" height="315" src= "https://www.youtube.com/embed/txMuWyhHUPk?si=0eDDyRwneEPDSbgw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
